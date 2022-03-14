b65c6a5d9e512c48eb47b5a85a68ef69fe9f3068
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-03-14 14:27:45 -0300

    Playing with ranges

--- lab/ranges.cpp
+++ lab/ranges.cpp
@@ -2,6 +2,11 @@
 // Created by cesch on 3/14/2022.
 //
 #include <iostream>
+#include <iterator>
+#include <ranges>
+#include <vector>
+#include <concepts>
+#include <algorithm>
 
 // From the video: https://www.youtube.com/watch?v=3MBtLeyJKg0&t=475
 
@@ -9,55 +14,90 @@
 // https://en.cppreference.com/w/cpp/iterator/input_iterator
 // https://en.cppreference.com/w/cpp/utility/exchange
 // https://en.cppreference.com/w/cpp/named_req/MoveConstructible
+// https://en.cppreference.com/w/cpp/language/operators
 
 struct fib_generator
 {
-    std::size_t operator()()
-    {
-        auto temp = cur_;
-        cur_ = std::exchange(next_, cur_ + next_);
-        return temp;
-    }
+	std::size_t operator()()
+	{
+		auto temp = cur_;
+		cur_ = std::exchange(next_, cur_ + next_);
+		return temp;
+	}
 private:
-    std::size_t cur_ = 0;
-    std::size_t next_ = 1;
+	std::size_t cur_ = 0;
+	std::size_t next_ = 1;
 };
 
 struct fib_iterator
 {
-    using value_type = std::size_t;
-    using difference_type = std::ptrdiff_t;
+	using value_type = std::size_t;
+	using difference_type = std::ptrdiff_t;
+	using iterator_category = std::forward_iterator_tag;
 
-    const value_type &operator*() const
-    { return cur_; }
+	const value_type &operator*() const
+	{
+		return cur_;
+	}
 
-    fib_iterator &operator++()
-    {
-        cur_ = std::exchange(next_, cur_ + next_);
-        return *this;
-    }
-    void operator++(int){ ++*this; }
+	fib_iterator &operator++()
+	{
+		cur_ = std::exchange(next_, cur_ + next_);
+		return *this;
+	}
+
+	fib_iterator operator++(int i)
+	{
+		auto temp = *this;
+		++*this;
+		return temp;
+	}
+
+	bool operator==(const fib_iterator &) const = default;
 
 private:
-    value_type cur_ = 0;
-    value_type next_ = 1;
+	value_type cur_ = 0;
+	value_type next_ = 1;
 };
+
+namespace views
+{
+constexpr auto fibonacci = std::ranges::subrange<fib_iterator, std::unreachable_sentinel_t>{};
+}
+
+void fibfun(int n, int k) {
+
+	auto view = views::fibonacci
+		| std::views::filter([k](auto i) {return i % k == 0;})
+		| std::views::take(n);
+
+	std::ranges::copy(view, std::ostream_iterator<std::size_t>(std::cout, " ,"));
+}
 
 auto main() -> int
 {
+	// Check concept at compile time
+	static_assert(std::is_integral_v<int>);
+	static_assert(std::input_iterator<fib_iterator>);
+//	static_assert(std::is_move_constructible_v<QString>);
+	//static_assert(std::is_move_constructible_v<QObject>);
+	//static_assert(std::is_move_constructible_v<QWidget>);
 
-    // Check concept at compile time
-    static_assert(std::is_integral_v<int>);
-    static_assert(std::input_iterator<fib_iterator>);
+	auto gen = fib_generator();
 
+	std::cout << gen.operator()() << std::endl;
+	std::cout << gen() << std::endl;
+	std::cout << gen() << std::endl;
+	std::cout << gen() << std::endl;
+	std::cout << gen() << std::endl;
 
-    auto gen = fib_generator();
+	auto iter = fib_iterator();
+	iter.operator++(10);
+	iter.operator++();
+	auto i = *iter;
+	iter++;
 
-    std::cout << gen() << std::endl;
-    std::cout << gen() << std::endl;
-    std::cout << gen() << std::endl;
-    std::cout << gen() << std::endl;
-    std::cout << gen() << std::endl;
+	fibfun(5, 3);
 
-    return 0;
+	return 0;
 }
