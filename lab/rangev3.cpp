ddabcd8fac34d7e7e89e0595b42c61f527bec8d6
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-03-15 09:39:53 -0300

    Add cpp20 random generation example.

--- lab/rangev3.cpp
+++ lab/rangev3.cpp
@@ -20,6 +20,7 @@ auto hello()
 	ranges::for_each(s, [](char c)
 	{ cout << c << ' '; });
 	cout << '\n';
+
 };
 
 auto is_six = [](int i)
@@ -136,6 +137,33 @@ auto find()
 			cout << "*i: " << *i;
 		}
 	}
+	cout << '\n';
+}
+
+auto forEach()
+{
+	auto print = [](int i)
+	{ cout << i << ' '; };
+
+	cout << "vector:   ";
+	std::vector<int> v{1, 2, 3, 4, 5, 6};
+	ranges::for_each(v, print); // 1 2 3 4 5 6
+
+	cout << "\narray:    ";
+	std::array<int, 6> a{1, 2, 3, 4, 5, 6};
+	ranges::for_each(a, print);
+
+	cout << "\nlist:     ";
+	std::list<int> ll{1, 2, 3, 4, 5, 6};
+	ranges::for_each(ll, print);
+
+	cout << "\nfwd_list: ";
+	std::forward_list<int> fl{1, 2, 3, 4, 5, 6};
+	ranges::for_each(fl, print);
+
+	cout << "\ndeque:    ";
+	std::deque<int> d{1, 2, 3, 4, 5, 6};
+	ranges::for_each(d, print);
 	cout << '\n';
 }
 
