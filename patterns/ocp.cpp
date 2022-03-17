fbc91b0f1da9636368ec68939b827d5f7d7c1e5c
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-03-17 11:45:53 -0300

    Nothing

--- patterns/ocp.cpp
+++ patterns/ocp.cpp
@@ -4,6 +4,18 @@
 
 // Open-Closed Principle
 
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <boost/lexical_cast.hpp>
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
+using std::ofstream;
+
 int main() {
 
 	return 0;
