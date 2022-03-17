3ba22957c3fc0cd08655100368c02aee9892b27b
Guilherme Ceschiatti B. Moreira <ceschiatti@gmail.com>
2022-03-17 12:16:30 -0300

    Adding Open-Closed Principle pattern.

--- patterns/ocp.cpp
+++ patterns/ocp.cpp
@@ -16,7 +16,46 @@ using std::cout;
 using std::endl;
 using std::ofstream;
 
-int main() {
+enum class Color
+{
+	red, green, blue
+};
+enum class Size
+{
+	small, medium, large
+};
+
+struct Product
+{
+	string name;
+	Color color;
+	Size size;
+};
+
+struct ProductFilter
+{
+	static auto by_color(const vector<Product *> &items, Color color) -> vector<Product *>
+	{
+		auto result = vector<Product *>{};
+
+		for (auto i : items)
+			if (i->color == color)
+				result.push_back(i);
+
+		return result;
+	}
+};
+
+int main()
+{
+	auto apple = Product{"Apple", Color::green, Size::small};
+	auto tree = Product{"Tree", Color::green, Size::large};
+	auto house = Product{"Hose", Color::blue, Size::large};
+
+	auto items = vector<Product *>{&apple, &tree, &house};
+
+	for (auto p : ProductFilter::by_color(items, Color::green))
+		cout << p->name << endl;
 
 	return 0;
 }
